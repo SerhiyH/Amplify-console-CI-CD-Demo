7ed920cc4a70f066f77d4d869b0a9f51a5385fb9
Serhiy Honcharov <honcharov.s@gmail.com>
2022-05-28 17:04:35 +0300

    Initial project version

---
+++ Documents/doc/goethe_university/uni_SoS21/Vorlesungen/ARA-SoS21/Übungen/0_13.04.21/Code/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+int main ()
+{
+    printf("%lu Byte\n", sizeof(char));
+    printf("%lu Byte\n", sizeof(int)); 
+    printf("%lu Byte\n", sizeof(float));
+    printf("%lu Byte\n", sizeof(double));
+
+
+    
+    return 0; 
+}
